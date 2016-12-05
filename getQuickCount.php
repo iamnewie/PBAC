c433dd79e92ad8035d2b7debc2ecf2ff691e1445
iamnewie <iamnewie@gmail.com>
2016-12-06 01:39:01 +0700

    update barchart;

--- getQuickCount.php
+++ getQuickCount.php
@@ -21,16 +21,29 @@
     // Check jika semua calon telah login
 
     if ($isLogged == '4') {
-        $query = 'select count(options) AS Count from QRCodeSuratSuara where options=1';
+        $query = 'select count(options) AS calon1 from QRCodeSuratSuara where options=1';
         $result = $conn->query($query);
-        $row = $result->fetch_assoc();
-        $suaraPasanganSatu = $row['Count'];
+        $arrayCalonSatu = array('calon1'=>$result->fetch_assoc()['calon1']);
 
-        $query = 'select count(options) AS Count from QRCodeSuratSuara where options=2';
+        //$suaraPasanganSatu = $row['Count'];
+
+        $query = 'select count(options) AS calon2 from QRCodeSuratSuara where options=2';
         $result = $conn->query($query);
-        $row = $result->fetch_assoc();
-        $suaraPasanganDua = $row['Count'];
-        echo "$suaraPasanganSatu;$suaraPasanganDua";
+        $arrayCalonDua = array('calon2'=>$result->fetch_assoc()['calon2']);
+        //$suaraPasanganDua = $row['Count'];
+
+        $query = 'select account.lokasi as lokasi,count(accountid) as countlokasi from qrcodesuratsuara right join account on account.userID = qrcodesuratsuara.accountID group by lokasi';
+        $result = $conn->query($query);
+        $arrayLokasiCount = array();
+        while($row = $result->fetch_assoc()){
+            $arrayLokasi = array($row['lokasi']=>$row['countlokasi']);
+            array_push($arrayLokasiCount,$arrayLokasi);
+        }
+
+        $arrayChart = array('lokasicount'=>$arrayLokasiCount);
+
+        echo json_encode(array_merge($arrayCalonSatu,$arrayCalonDua,$arrayChart));
+
     } else {
         echo "lock";
     }
